06435d4aaad85447ac512766fb86f58c7ded7a25
jules-crevoisier <crevoisier.ju@gmail.com>
2026-01-22 15:26:05 +0100

    Add Link component import to booking page

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
